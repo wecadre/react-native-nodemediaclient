66aea036596fc5598cf84228a3ff62fd26c7518d
Dhoncrisley Lins <dhoncrisley.lins@alive.app.br>
2021-02-23 09:54:09 -0300

    add status consts

--- index.js
+++ index.js
@@ -7,4 +7,22 @@
 import NodeCameraView from './NodeCameraModule';
 import NodePlayerView from './NodePlayerModule';
 import { NativeModules } from 'react-native';
-module.exports = { NodeCameraView, NodePlayerView, NodeMediaClient: NativeModules.NodeMediaClient };
+module.exports = {
+    NodeCameraView,
+    NodePlayerView,
+    NodeMediaClient: NativeModules.NodeMediaClient,
+    OutputStreamStatus: {
+        Connecting = 2000,
+        Start = 2001,
+        Failed = 2002,
+        Closed = 2004,
+        None = 0,
+    },
+    InputStreamStatus: {
+        Connecting = 1000,
+        Connected = 1001,
+        Buffering = 1101,
+        BufferFull = 1102,
+        Resolution = 1104,
+    }
+};
